45aa89214019d3ea7850cb0e4ca58a165b2343a1
Emily (Chisholm) O'Grady <38440373+EOGrady21@users.noreply.github.com>
2025-06-26 11:52:40 -0300

    Update test-plot-time-series.R
    improve test documentaiton

--- tests/testthat/test-plot-time-series.R
+++ tests/testthat/test-plot-time-series.R
@@ -1,56 +1,51 @@
-# Example test, plotting function not tailored yet for marea. TODO. See pacea
-# for more examples, just doing some simple ones here.
+# Tests for time series plotting functions in marea
+#
+# These tests check that plotting functions for indices and AZMP bottom temperature
+# behave as expected, including error handling and various plotting options.
+# For more comprehensive examples, see the pacea package.
 
-# plot.pacea_index()   # Need to export somehow, this just uses the default plot
-# that gives a correlation plot, but satisfies tests.
-# plot.pacea_index()
-test_that("index plotting: the stopifnot commands are working", {
-  expect_error(plot(oni,
-                    value = "foo"))
+test_that("index plotting: stopifnot error handling works", {
+  expect_error(plot(oni, value = "foo"))
 })
 
 test_that("index plotting works with various options", {
-  expect_invisible(plot(oni,
-                        style = "red_blue"))
+  expect_invisible(plot(oni, style = "red_blue"))
 
   expect_invisible(plot(oni,
-                        xlim = c(lubridate::dmy(01011950),
-                                 lubridate::dmy(01012040)))) # to expand x-axis
-  expect_invisible(plot(oni,
-                       type = "o"))     # passed onto plot()
-  expect_invisible(plot(oni,
-                        main = "My nice plot"))  # passed onto plot()
+    xlim = c(lubridate::dmy(01011950), lubridate::dmy(01012040))
+  )) # expand x-axis
 
-  # Not quite sure why these two aren't working when they do for pacea.
+  expect_invisible(plot(oni, type = "o")) # pass type to plot()
+  expect_invisible(plot(oni, main = "My nice plot")) # pass main to plot()
+
+  # The following tests are commented out because they do not currently work in marea,
+  # but do work in pacea. Uncomment and adapt as needed if/when functionality is added.
 
   # expect_invisible(plot(oni,
-  #                       event_years = c(2007, 2008, 2010),
-  #                       value = "value",
-  #                       style = "plain"))
+  #   event_years = c(2007, 2008, 2010),
+  #   value = "value",
+  #   style = "plain"
+  # ))
 
   # expect_invisible(plot(oni,
-  #                       event_lub = c(lubridate::dmy(02081972),
-  #                                    lubridate::dmy(31121999))))
+  #   event_lub = c(lubridate::dmy(02081972), lubridate::dmy(31121999))
+  # ))
 
-  suppressWarnings(expect_warning(plot(oni,
-                                       not_an_option = "My nice plot")))
-                                       # nonsensical ... option
+  suppressWarnings(expect_warning(
+    plot(oni, not_an_option = "My nice plot"),
+    regexp = NA # Accept any warning, as this is a nonsensical ... option
+  ))
 
-  expect_invisible(plot(oni,
-                        style = "goa"))   # that isn't implemented yet
-
+  expect_invisible(plot(oni, style = "goa")) # not implemented yet, should not error
 })
 
+test_that("Plotting AZMP Bottom Temperature", {
+  expect_invisible(plot(azmp_bottom_temperature, value = "mean"))
 
-test_that("Plotting AZMP Bottom Temperature ", {
-  expect_invisible(plot(azmp_bottom_temperature, value="mean"))
-  
-  expect_invisible(plot(azmp_bottom_temperature, value="mean", region=c("4X", "4W")))
+  expect_invisible(plot(azmp_bottom_temperature, value = "mean", region = c("4X", "4W")))
 
   expect_error(
     plot(azmp_bottom_temperature, value = "mean", region = "hello"),
     "region must be in Cabot Strait,E Georges Bank,Emerald Basin,Georges Basin,Lurcher Shoal,Misaine Bank,4Vn,4Vs,4W,4X"
   )
- 
-  
 })
