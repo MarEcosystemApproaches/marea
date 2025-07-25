29bb8a417b0bac7f76728e3c74088093bc9afe10
EOGrady21 <38440373+EOGrady21@users.noreply.github.com>
2025-07-24 21:03:57 -0300

    S4 update

---
+++ vignettes/plotting_ea.R
@@ -0,0 +1,14 @@
+## ----echo = FALSE-------------------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>",
+  fig.path = "man/figures/README-",
+  out.width = "100%"
+)
+
+## ----echo = FALSE, message = FALSE, warning = FALSE---------------------------
+library(marea)
+library(ggplot2)
+library(tidyverse)
+library(knitr)
+
