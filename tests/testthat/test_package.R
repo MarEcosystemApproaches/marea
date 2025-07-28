29ef5fbef44d6bf474781e920da5a2bde39d8234
EOGrady21 <38440373+EOGrady21@users.noreply.github.com>
2025-07-28 11:55:42 -0300

    testing package

---
+++ tests/testthat/test_package.R
@@ -0,0 +1,14 @@
+library(testthat)
+library(marea)
+testthat::test_that("marea", {
+  # Load the package
+  library(marea)
+
+  # Test if the package loads correctly
+  expect_true("marea" %in% rownames(installed.packages()))
+
+  # Test if a specific function exists
+  expect_true(exists("marea_metadata", where = "package:marea"))
+
+
+})
