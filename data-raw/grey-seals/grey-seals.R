a0e5a49e04397106114464e1ed11bd0a1bbc492d
EOGrady21 <38440373+EOGrady21@users.noreply.github.com>
2025-09-18 11:50:01 -0300

    update citation for grey seals

--- data-raw/grey-seals/grey-seals.R
+++ data-raw/grey-seals/grey-seals.R
@@ -44,7 +44,7 @@ grey_seals <- ea_data(
   species = "grey seal",
   region = "Scotian Shelf",
   location_descriptor = 'Sable Island',
-  source_citation = "Hammill et al. (2023)",
+  source_citation = "den Heyer, C. E., Mosnier, A., Stenson, G. B., Lidgard, D. C., Bowen, W. D., & Hammill, M. O. (2024). Grey seal pup production in Canada (DFO Can. Sci. Advis. Sec. Res. Doc. 2023/078). Fisheries and Oceans Canada, Canadian Science Advisory Secretariat.",
   comment = paste0("Estimates from a Bayesian state-space model fitted to aerial survey data and demographic rates from Sable Island. See Hammill et al. (2023) for details.")
 )
 
