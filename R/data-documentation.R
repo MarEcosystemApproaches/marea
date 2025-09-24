b376883557c946d24d675eef0caa121ed5d589d6
Clay <Stephanie.Clay@dfo-mpo.gc.ca>
2025-09-24 09:43:59 -0300

    - add function to download satellite ocean colour data from erddap - add script to download, filter, format, and save satellite data as rda - add satellite ocean colour rda file - add documentation for satellite ocean colour product

--- R/data-documentation.R
+++ R/data-documentation.R
@@ -524,3 +524,32 @@
 #' @author Jamie C. Tam
 #' @source Generated from running `data-raw/eco-indicators/eco-indicators.R`
 "eco_indicators"
+
+
+#' Satellite Ocean Colour (ea_spatial)
+#'
+#' Annual spring bloom metrics and average chlorophyll-a derived from the OC-CCI POLY4 chl-a product in the Northwest Atlantic. Pulled using marea::get_erddap_data(). NRMSE_bloom > 0.5 and |standardized anomalies| > 4 filtered out. Resampled from 4.64 km to 0.1 degree resolution.
+#' 
+#' @format An `ea_spatial` (stars) object:
+#' \describe{
+#'   \item{t_start_value}{Day of year of the start of the spring phytoplankton bloom (day of year)}\
+#'   \item{t_duration_value}{Duration of the spring phytoplankton bloom (days)}\
+#'   \item{amplitude_real_value}{Maximum concentration during the spring phytoplankton bloom period (mg/m3)}\
+#'   \item{magnitude_real_value}{Total chlorophyll-a produced during the spring phytoplankton bloom period (days*mg/m3)}\
+#'   \item{annual_mean_value}{Average chlorophyll-a over the year (mg/m3)}\
+#' }
+#' \strong{Metadata (\code{attr(obj, "meta")})}:
+#' \describe{
+#'   \item{data_type}{Character. "t_start: Day of year of the start of the spring phytoplankton bloom; t_duration: Duration of the spring phytoplankton bloom; amplitude_real: Maximum concentration during the spring phytoplankton bloom period; magnitude_real: Total chlorophyll-a produced during the spring phytoplankton bloom period; annual_mean: Average chlorophyll-a over the year"}
+#'   \item{units}{Character. "t_start: day of year; t_duration: days; amplitude_real: mg/m3; magnitude_real: days*mg/m3; annual_mean: mg/m3"}
+#'   \item{region}{Character. "Northwest Atlantic"}
+#'   \item{time_descriptor}{Character. Year}
+#'   \item{source_citation}{Character. "Clay, S., & Devred, E. (2025). Northwest Atlantic per-pixel spring bloom metrics derived from OC-CCI POLY4 satellite Chlorophyll-a (v1.1) [Data set]. https://catalogue.cioosatlantic.ca/dataset/ca-cioos_473fe40d-613e-4aba-b335-7a31a04e5d69?local=en"}
+#'   \item{temporal_coverage}{Character. "1998-2024"}
+#'   \item{variable_name}{Character. The name used for the variable}
+#' }
+#' @name satellite_ocean_colour
+#' @docType data
+#' @author marea/DFO
+#' @source SOPhyE group, DFO, see data-raw/satellite-ocean-colour/satellite_ocean_colour.R
+"satellite_ocean_colour"
--- data-raw/satellite-ocean-colour/satellite_ocean_colour.R
+++ data-raw/satellite-ocean-colour/satellite_ocean_colour.R
@@ -1,38 +1,40 @@
 rm(list=ls())
 library(rerddap)
-library(terra)
 library(sf)
 library(dplyr)
 library(lubridate)
 library(marea)
+library(stars)
 
 # bloom metrics available in the satellite product
-metrics <- c("t_start","t_duration","amplitude_real","magnitude_real","annual_mean","NRMSE_bloom","percent_dineof")
+metrics <- list(t_start = list(data_type="Day of year of the start of the spring phytoplankton bloom",
+                               units="day of year"),
+                t_duration = list(data_type="Duration of the spring phytoplankton bloom",
+                                  units="days"),
+                amplitude_real = list(data_type="Maximum concentration during the spring phytoplankton bloom period",
+                                      units="mg/m3"),
+                magnitude_real = list(data_type="Total chlorophyll-a produced during the spring phytoplankton bloom period",
+                                      units="days*mg/m3"),
+                annual_mean = list(data_type="Average chlorophyll-a over the year",
+                                   units="mg/m3"),
+                NRMSE_bloom = list(data_type="Root mean square error between the fitted Gaussian and real chl-a values during the bloom period, normalized to amplitude_real",
+                                   units="mg/m3"),
+                percent_dineof = list(data_type="Percentage of days with pixel values estimated using DINEOF",
+                                      units=""))
 
-#**************************
-# get the dataset into from the server
-dataset_info <- rerddap::info("bio_remote_sensing_occci_nwa_poly4_spring_bloom", url="https://cioosatlantic.ca/erddap/")
 
-#**************************
-# get dataset boundaries from erddap
-xlim <- as.numeric(strsplit(dataset_info$alldata$longitude$value[3],split=", ")[[1]])
-ylim <- as.numeric(strsplit(dataset_info$alldata$latitude$value[3],split=", ")[[1]])
-zlim <- format(as_datetime(as.numeric(strsplit(dataset_info$alldata$time$value[3],split=", ")[[1]])),"%Y-%m-%dT00:00:00Z")
-field <- "all"
+#*******************************************************************************
+# DOWNLOAD AND FILTER ####
 
-#**************************
-# download the data from the server
-df <- griddap(dataset_info, latitude=ylim, longitude=xlim, time=zlim, fields=field)$data %>%
-  dplyr::mutate(time=as_datetime(time))
+# download the full dataset
+df <- get_erddap_data(variables=names(metrics))
 
-#**************************
-# FILTER OUT NRMSE_BLOOM > 0.5
+# filter out NRMSE_bloom > 0.5
 bad_inds <- df$NRMSE_bloom > 0.5
 bad_inds[!is.finite(bad_inds)] <- TRUE
 df[bad_inds,c("t_start","t_duration","amplitude_real","magnitude_real","NRMSE_bloom")] <- NA
 
-#**************************
-# FILTER OUT |STANDARDIZED_ANOMALY| > 4
+# filter out |standardized_anomaly| > 4
 stdevnum <- 4
 dfsa <- df %>%
   dplyr::group_by(longitude,latitude) %>%
@@ -57,43 +59,91 @@ dfsa <- df %>%
                                          abs(annual_mean_sanom)>stdevnum))
 bad_inds <- dfsa$anySAGTstdevnum
 bad_inds[!is.finite(bad_inds)] <- TRUE
-df[bad_inds,metrics] <- NA
+df[bad_inds,names(metrics)] <- NA
 
-#**************************
-# Convert to ea_spatial
+# make sure lat/lon columns are in the correct format
+df <- df %>% dplyr::mutate(longitude=as.numeric(longitude),
+                           latitude=as.numeric(latitude))
 
-for (metric in metrics) {
-  
-  times <- sort(unique(df$time))
-  
-  # convert the data for this metric into a multilayer spatraster (each layer in a single time unit)
-  r <- lapply(times, function(ti) {
-    df %>%
-      dplyr::filter(time==ti) %>%
-      dplyr::select(longitude,latitude,all_of(metric)) %>%
-      dplyr::mutate(longitude=as.numeric(longitude), latitude=as.numeric(latitude)) %>%
-      tidyterra::as_spatraster(xycols=1:2,crs="EPSG:4326")
-  }) %>% do.call(what=c) %>% setNames(paste0(names(.),"_",year(as_date(times))))
-  time(r) <- times
-  
-  r_ea_spatial <- as_ea_spatial(
-    x = r,
-    value_col = names(r),
-    data_type    = "Phytoplankton spring bloom initiation day",
-    region       = "Northwest Atlantic",
-    time_descriptor = "Annual",
-    units        = "Day of year",
-    source_citation = "SOPhyE group, DFO",
-    temporal_coverage = paste0(year(range(time(r))),collapse="-"),
-    variable_name= metric
-  )
-  # Assign the object to a variable with the name from metric
-  # TODO @ steph  maybe preface the name with something consistent so data object can be 
-  # grouped together? ie. paste0('sophye_', metric) @
-  assign(metric, r_ea_spatial)
-  
-  # Save the data with the metric name
-  do.call(usethis::use_data, list(as.name(metric), overwrite = TRUE, internal = FALSE))
-  
-  }
+
+#*******************************************************************************
+# WRITE TO OUTPUT ####
+
+# metrics to include in the output
+metrics_output <- names(metrics)[!(names(metrics) %in% c("NRMSE_bloom","percent_dineof"))]
+
+# variables for ea_spatial attributes
+temporal_cov <- paste0(year(range(df$time)),collapse="-")
+data_types <- sapply(metrics_output, function(m) paste0(m,": ",metrics[[m]]$data_type)) %>%
+  paste0(collapse="; ")
+units <- sapply(metrics_output, function(m) paste0(m,": ",metrics[[m]]$units)) %>%
+  paste0(collapse="; ")
+
+# convert the data for this metric into a stars object and reduce resolution to 0.1 degrees
+r <- df %>%
+  dplyr::select(longitude,latitude,time,all_of(metrics_output)) %>% 
+  st_as_stars(dims = c("longitude","latitude","time")) %>%
+  st_set_crs("EPSG:4326") %>%
+  st_warp(dest=st_as_stars(st_bbox(.), dx=0.1, method="med"))
+
+# convert to ea_spatial
+reasp <- as_ea_spatial(
+  x = r,
+  value_col = metrics_output,
+  data_type    = data_types,
+  units        = units,
+  region       = "Northwest Atlantic",
+  time_descriptor = "Annual",
+  source_citation = "SOPhyE group, DFO",
+  temporal_coverage = temporal_cov,
+  variable_name = paste0(metrics_output,collapse=", ")
+)
+
+# Assign the object to a variable with the name from metric
+assign("satellite_ocean_colour", reasp)
+
+# Save the data with the metric name
+do.call(usethis::use_data, list(as.name("satellite_ocean_colour"), overwrite = TRUE, internal = FALSE))
+
+
+# # quick test - plotting t_start in 2007
+# data(satellite_ocean_colour)
+# times <- stars::st_get_dimension_values(satellite_ocean_colour@data, "time")
+# plot(satellite_ocean_colour@data["t_start_value",,,which(times==lubridate::as_date("20070101"))])
+
+
+# # ALTERNATIVE: SAVE EACH VARIABLE INDIVIDUALLY
+# 
+# for (metric in metrics_output) {
+# 
+#   # grab the metric info, data, and temporal coverage
+#   metric_info <- metrics[[metric]]
+#   tmp_df <- df %>% dplyr::select(longitude,latitude,time,all_of(metric))
+#   temporal_cov <- paste0(year(range(tmp_df$time)),collapse="-")
+# 
+#   # convert the data for this metric into a stars object and reduce resolution to 0.1 degrees
+#   r <- st_as_stars(tmp_df, dims = c("longitude","latitude","time")) %>%
+#     st_set_crs("EPSG:4326") %>%
+#     st_warp(dest=st_as_stars(st_bbox(.), dx=0.1, method="med"))
+#   
+#   # convert to ea_spatial
+#   reasp <- as_ea_spatial(
+#     x = r,
+#     value_col = metric,
+#     data_type    = metric_info$data_type,
+#     units        = metric_info$units,
+#     region       = "Northwest Atlantic",
+#     time_descriptor = "Annual",
+#     source_citation = "SOPhyE group, DFO",
+#     temporal_coverage = temporal_cov,
+#     variable_name = metric
+#   )
+# 
+#   # Assign the object to a variable with the name from metric
+#   assign(paste0("sophye_RR_",metric), reasp)
+# 
+#   # Save the data with the metric name
+#   do.call(usethis::use_data, list(as.name(paste0("sophye_",metric)), overwrite = TRUE, internal = FALSE))
+# 
+# }
 
