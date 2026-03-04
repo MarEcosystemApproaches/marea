99e6a0f6d8080bc3d9ed89da5e05d975a1b1c061
Remi Daigle <Remi.Daigle@dfo-mpo.gc.ca>
2026-03-04 15:34:56 -0400

    fix python installation issues fixes It appears miniconda is blocked on DFO machines Fixes #92

--- R/get_CMEMS_ncdf.R
+++ R/get_CMEMS_ncdf.R
@@ -22,15 +22,69 @@
 #' \dontrun{
 #' get_CMEMS_ncdf(username = "your_username", password = "your_password", variables = list("thetao"))
 #' }
-get_CMEMS_ncdf <- function(username = NA, password = NA, dataset_id = "cmems_mod_glo_phy_my_0.083deg_P1M-m", variables, minimum_longitude = -67.74250, maximum_longitude = -54.90132, minimum_latitude = 40.04343, maximum_latitude = 47.83333, start_datetime = "1993-12-01T00:00:00", end_datetime = "1994-12-01T00:00:00", output_filename = tempfile(fileext = ".nc")) {
-  pythonenv <- try(reticulate::use_virtualenv("CopernicusMarine", required = TRUE))
-
-  if (inherits(pythonenv, "try-error")) {
-    reticulate::virtualenv_create(envname = "CopernicusMarine")
-    reticulate::virtualenv_install("CopernicusMarine", packages = c("copernicusmarine"))
+get_CMEMS_ncdf <- function(
+  username = NA,
+  password = NA,
+  dataset_id = "cmems_mod_glo_phy_my_0.083deg_P1M-m",
+  variables,
+  minimum_longitude = -67.74250,
+  maximum_longitude = -54.90132,
+  minimum_latitude = 40.04343,
+  maximum_latitude = 47.83333,
+  start_datetime = "1993-12-01T00:00:00",
+  end_datetime = "1994-12-01T00:00:00",
+  output_filename = tempfile(fileext = ".nc")
+) {
+  # Check if Python is available
+  if (!reticulate::py_available()) {
+    stop(
+      "Python is not installed. Please install Python from https://www.python.org or contact your system administrator.",
+      call. = FALSE
+    )
   }
-  reticulate::use_virtualenv("CopernicusMarine", required = TRUE)
-  cmt <- try(import("copernicusmarine"))
+
+  # Try to use virtual environment first
+  venv_setup_success <- FALSE
+  pythonenv <- tryCatch(
+    {
+      reticulate::use_virtualenv("CopernicusMarine", required = TRUE)
+      venv_setup_success <- TRUE
+      TRUE
+    },
+    error = function(e) {
+      message("Virtual environment not found. Attempting to create one...")
+      tryCatch(
+        {
+          reticulate::virtualenv_create(envname = "CopernicusMarine")
+          reticulate::virtualenv_install(
+            "CopernicusMarine",
+            packages = c("copernicusmarine")
+          )
+          reticulate::use_virtualenv("CopernicusMarine", required = TRUE)
+          venv_setup_success <<- TRUE
+          TRUE
+        },
+        error = function(e2) {
+          message(
+            "Virtual environment creation failed. Falling back to system Python with pip..."
+          )
+          FALSE
+        }
+      )
+    }
+  )
+
+  # If virtual environment setup failed, use pip install on system Python
+  if (!venv_setup_success) {
+    message("Using system Python installation with pip...")
+    tryCatch(
+      reticulate::import("copernicusmarine"),
+      error = function(e) {
+        message("Installing copernicusmarine via pip...")
+        reticulate::py_install("copernicusmarine", pip = TRUE)
+      }
+    )
+  }
 
   # Login function to create your configuration file
   if (!is.na(username) | !is.na(password)) {
