a352dc6d0f6e6725aaa5bdfd41e696e43864819d
EOGrady21 <38440373+EOGrady21@users.noreply.github.com>
2026-03-13 10:21:01 -0300

    link azmpdata carbonate

--- data-raw/carbonate/carbonate-data.R
+++ data-raw/carbonate/carbonate-data.R
@@ -25,21 +25,35 @@ make_ea_index <- function(
 }
 
 carbonate_new <- tibble(
-  year   = azmpdata::,
-  region = azmpdata::,
-  mean   = azmpdata::
-) %>%
-  filter(!is.na(mean))
+  year   = azmpdata::Derived_Annual_Carbonate$year,
+  section = azmpdata::Derived_Annual_Carbonate$section_name,
+  depth = azmpdata::Derived_Annual_Carbonate$depth_m,
+  TA_mean_umolkg   = azmpdata::Derived_Annual_Carbonate$mean_TA_umolkg,
+  DIC_mean_umolkg  = azmpdata::Derived_Annual_Carbonate$mean_DIC_umolkg,
+  pH_mean         = azmpdata::Derived_Annual_Carbonate$mean_pH_total,
+  pco2_mean_uatm   = azmpdata::Derived_Annual_Carbonate$mean_pCO2_uatm,
+  substrate_inhibitor_ratio = azmpdata::Derived_Annual_Carbonate$mean_substrate_inhibitor_ratio,
+  omega_aragonite_mean = azmpdata::Derived_Annual_Carbonate$mean_omega_aragonite,
+  omega_calcite_mean = azmpdata::Derived_Annual_Carbonate$mean_omega_calcite,
+  carbonate_system_vulnerability_index = azmpdata::Derived_Annual_Carbonate$mean_carbonate_system_vulnerability_index
+) 
 
 azmp_carbonate <- make_ea_index(
   df = carbonate_new,
-  value_col = c("mean", "others"),
+  value_col = c("TA_mean_umolkg", 
+                "DIC_mean_umolkg", 
+                "pH_mean", 
+                "pco2_mean_uatm",
+                "substrate_inhibitor_ratio",
+                "omega_aragonite_mean",
+                "omega_calcite_mean",
+                "carbonate_system_vulnerability_index"),
   data_type = "Synthesized Carbonate Indices",
   region = "Scotian Shelf (4X, 4V, 4W)",
   location = "Maritimes region",
-  units = "list all variable units",
+  units = "TA = umol/kg, DIC = umol/kg, pH = total scale, pCO2 = uatm, substrate-inhibitor ratio = unitless, omega aragonite = unitless, omega calcite = unitless, carbonate system vulnerability index = unitless",
   source = "DFO Atlantic Zone Monitoring Program via azmpdata package (https://casaultb.github.io/azmpdata/)",
   principal_investigator = "Kumiko Azetsu-Scott (Kumiko.Azetsu-Scott@dfo-mpo.gc.ca)",
   citation = "Lizotte, M., Blais, M., Chassé, J., Galbraith, P. S., Hébert, A.-J., Starr, M. 2026. Acidification and CO2-Driven Conditions in the Estuary and Gulf of St. Lawrence During 2024. Can. Tech. Rep. Hydrogr. Ocean. Sci. 410 : vi + 71 p."
 )
-usethis::use_data(azmp_bottom_temperature, overwrite = TRUE)
+usethis::use_data(azmp_carbonate, overwrite = TRUE)
